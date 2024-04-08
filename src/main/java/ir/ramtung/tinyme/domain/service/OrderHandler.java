1d5c134a3405333f2b9c103a497938b122e17dea
hatef2003 <hatef.1382@gamil.com>
2024-04-08 11:02:53 +0330

    order handler changed

--- src/main/java/ir/ramtung/tinyme/domain/service/OrderHandler.java
+++ src/main/java/ir/ramtung/tinyme/domain/service/OrderHandler.java
@@ -56,6 +56,10 @@ public class OrderHandler {
                 eventPublisher.publish(new OrderRejectedEvent(enterOrderRq.getRequestId(), enterOrderRq.getOrderId(), List.of(Message.SELLER_HAS_NOT_ENOUGH_POSITIONS)));
                 return;
             }
+            if (matchResult.outcome() == MatchingOutcome.NOT_ENOUGH_TRADE)
+            {
+                eventPublisher.publish(new OrderRejectedEvent(enterOrderRq.getRequestId(), enterOrderRq.getOrderId(), List.of(Message.THERE_WAS_NOT_ENOUGH_TRADES)));
+            }
 
             if (enterOrderRq.getRequestType() == OrderEntryType.NEW_ORDER) {
                 eventPublisher.publish(new OrderAcceptedEvent(enterOrderRq.getRequestId(), enterOrderRq.getOrderId()));
