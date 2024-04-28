3ac8680d7f0ae5be624d7b9c02340f121a653635
hatef2003 <hatef.1382@gamil.com>
2024-04-28 14:35:46 +0330

    Test added

--- src/main/java/ir/ramtung/tinyme/domain/entity/OrderCancellationQueue.java
+++ src/main/java/ir/ramtung/tinyme/domain/entity/OrderCancellationQueue.java
@@ -1,4 +1,5 @@
 package ir.ramtung.tinyme.domain.entity;
+
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -7,25 +8,30 @@ import java.util.stream.Stream;
 
 @Getter
 public class OrderCancellationQueue {
-    ArrayList<StopLimitOrder> deactivatedBuyOrders ;
-    ArrayList<StopLimitOrder> deactivatedSellOrders ;
-    OrderCancellationQueue()
-    {
+    ArrayList<StopLimitOrder> deactivatedBuyOrders;
+    ArrayList<StopLimitOrder> deactivatedSellOrders;
+
+    OrderCancellationQueue() {
         deactivatedBuyOrders = new ArrayList<>();
         deactivatedSellOrders = new ArrayList<>();
     }
+
     public StopLimitOrder findStopLimitOrderById(long id) {
-        Stream<StopLimitOrder> joinedDeactivatedList = Stream.concat(deactivatedBuyOrders.stream(), deactivatedSellOrders.stream());
-        return joinedDeactivatedList.filter(order->order.getOrderId() == id).findFirst().orElse(null);
+        Stream<StopLimitOrder> joinedDeactivatedList = Stream.concat(deactivatedBuyOrders.stream(),
+                deactivatedSellOrders.stream());
+        return joinedDeactivatedList.filter(order -> order.getOrderId() == id).findFirst().orElse(null);
     }
+
     public void addToDeactivatedBuy(StopLimitOrder newOrder) {
         deactivatedBuyOrders.add(newOrder);
         deactivatedBuyOrders.sort(Comparator.comparingDouble(StopLimitOrder::getStopLimit));
     }
+
     public void addToDeactivatedSell(StopLimitOrder newOrder) {
         deactivatedSellOrders.add(newOrder);
         deactivatedSellOrders.sort(Comparator.comparingDouble(StopLimitOrder::getStopLimit).reversed());
     }
+
     public void removeFromDeactivatedList(long id) {
         StopLimitOrder order = findStopLimitOrderById(id);
         if (order != null) {
@@ -35,11 +41,10 @@ public class OrderCancellationQueue {
                 deactivatedSellOrders.remove(order);
         }
     }
-    public ArrayList<StopLimitOrder> getActivatedOrder(int lastTradePrice)
-    {
-        ArrayList<StopLimitOrder> activatedList = new ArrayList<>(); 
-        for (
-        StopLimitOrder order : Stream.concat(deactivatedBuyOrders.stream(),
+
+    public ArrayList<StopLimitOrder> getActivatedOrder(int lastTradePrice) {
+        ArrayList<StopLimitOrder> activatedList = new ArrayList<>();
+        for (StopLimitOrder order : Stream.concat(deactivatedBuyOrders.stream(),
                 deactivatedSellOrders.stream()).toList())
             if (order.isActive(lastTradePrice)) {
                 activatedList.add(order);
